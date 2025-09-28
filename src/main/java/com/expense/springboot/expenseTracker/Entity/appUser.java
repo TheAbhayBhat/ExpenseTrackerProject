e5945cfaebba5cf290f2b8235d8b5442bde043e4
Abhay Bhat <abhaybhat86@gmail.com>
2025-09-28 22:57:23 +0530

    this is comuut

--- src/main/java/com/expense/springboot/expenseTracker/Entity/appUser.java
+++ src/main/java/com/expense/springboot/expenseTracker/Entity/appUser.java
@@ -1,4 +1,16 @@
 package com.expense.springboot.expenseTracker.Entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Entity
+@Table(name="AppUser")
 public class appUser {
+    //"this is a commit"
 }
